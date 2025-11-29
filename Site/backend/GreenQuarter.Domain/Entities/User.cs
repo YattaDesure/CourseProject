58ecf795d2417efcf3faf23904d30df4582438a7
YattaDesure <edikyazikov@gmai.com>
2025-11-29 13:12:11 +0300

    clear commit

--- Site/backend/GreenQuarter.Domain/Entities/User.cs
+++ Site/backend/GreenQuarter.Domain/Entities/User.cs
@@ -10,10 +10,5 @@ public class User : IdentityUser
     public string Role { get; set; } = "User"; // User, Moderator, Admin
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsActive { get; set; } = true;
-
-    // Navigation properties
-    public ICollection<UserApartment> UserApartments { get; set; } = new List<UserApartment>();
-    public ICollection<UserParkingSpace> UserParkingSpaces { get; set; } = new List<UserParkingSpace>();
-    public ICollection<UserStorageRoom> UserStorageRooms { get; set; } = new List<UserStorageRoom>();
 }
 
